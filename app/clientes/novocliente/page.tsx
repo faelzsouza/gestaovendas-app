e55bb979aa9892768accf5da153a4af4f0a992c4
Rafael Martins <faelzsouza@gmail.com>
2023-07-26 12:00:32 -0300

    feat: add `novocliente` page

---
+++ app/clientes/novocliente/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Form from "@/components/form";
+import InputInfos from "@/types/inputInfos.type";
+
+export default function CadastroCliente() {
+    const inputs: InputInfos[] = [
+        {
+            label: "Nome",
+            type: "text",
+            id: "nome",
+            placeholder: "Digite o nome",
+        },
+        {
+            label: "Nascimento",
+            type: "date",
+            id: "nascimento",
+        },
+        {
+            label: "Telefone",
+            type: "tel",
+            id: "phone",
+        },
+    ];
+    return <Form inputInfos={inputs} />;
+}
